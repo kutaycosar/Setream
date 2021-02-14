4677c8ff1e761520a538349c69465509a6713c7b
Kutay <kutayc@gmail.com>
2021-02-14 10:22:28 +0300

    yorumlar liste goruntusu eklendi

--- app.js
+++ app.js
@@ -5,18 +5,18 @@ let app = express()
 let db
 let data 
 
-// let port = process.env.PORT
-// if (port == null || port == "") {
-//     port = 3000
-// }
-//hehehe
+let port = process.env.PORT
+if (port == null || port == "") {
+    port = 3000
+}
+
 app.use(express.static('public')) //public folderini kullanicaz
 
 let connectionString = 'mongodb://todoAppUser:0024882aaa@cluster0-shard-00-00.m1hmo.mongodb.net:27017,cluster0-shard-00-01.m1hmo.mongodb.net:27017,cluster0-shard-00-02.m1hmo.mongodb.net:27017/Setream?ssl=true&replicaSet=atlas-tj1li7-shard-0&authSource=admin&retryWrites=true&w=majority'
 
 mongodb.connect(connectionString, {useNewUrlParser: true}, function(err, client){
     db = client.db()
-    app.listen(3000) // db olusturuldugunda portu dinlemeye baslar
+    app.listen(port) // db olusturuldugunda portu dinlemeye baslar
 })
 
 app.use(express.json())
@@ -49,12 +49,14 @@ app.get('/yorumlar',passwordProtected, function(req,res){
 
 app.post('/create-item',function(req,res){
     date = new Date()
+    var x = 1
     data = {
         text: req.body.text,
         yorum: req.body.text2,
-        date: date.toLocaleString()
+        date: date.toLocaleString(),
+        no: x.toString()
     }
-    
+    x = x+1
     db.collection('items').insertOne(data, function(err, info){
         res.json(info.ops[0])
     })
